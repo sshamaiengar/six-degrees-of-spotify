3cbe3daf89258abde26f8747134d5e95f43ece52
Stephen Shamaiengar <s.shamaiengar@gmail.com>
2019-07-03 14:14:49 -0400

    Added API functions for searching for artists. Added artist information to connection response.

--- src/api.py
+++ src/api.py
@@ -8,14 +8,59 @@ app = Quart(__name__)
 
 
 # route for getting path given artist IDs
-@app.route('/<artist1_id>/<artist2_id>')
+@app.route('/api/connect/<artist1_id>/<artist2_id>', methods=['GET'])
 async def find_connections(artist1_id, artist2_id):
 	artist1: Artist = await clients.spotify.get_artist(artist1_id)
 	artist2: Artist = await clients.spotify.get_artist(artist2_id)
 	name_path, id_path, artists_searched = await bi_bfs(artist1, artist2)
-	res = {"artists": name_path, "ids": id_path, "count": artists_searched}
+	artist_dicts = []
+	for i in id_path:
+		artist_dict = await get_artist_dict(i)
+		artist_dicts.append(artist_dict)
+
+	# res = {"artists": name_path, "ids": id_path, "count": artists_searched}
+	res = artist_dicts
 
 	return Response(json.dumps(res), mimetype='text/json')
+
+# route for getting search results for web app
+@app.route('/api/search/<artist_name>', methods=['GET'])
+async def search_artists(artist_name):
+	results = await clients.spotify.search(artist_name, types=['artist'], limit=20)
+	artists: List[Artist] = results['artists']
+	artist_dicts: List[Dict] = []
+	for a in artists:
+		artist: Dict = {}
+		artist['name'] = a.name
+		artist['id'] = a.id
+		artist['images'] = a.images
+		artist_dicts.append(artist)
+	res = artist_dicts
+	return Response(json.dumps(res), mimetype='text/json')
+
+# route for getting one artist (after path found)
+@app.route('/api/artist/<artist_id>', methods=['GET'])
+async def get_artist(artist_id):
+	artist: Artist = await clients.spotify.get_artist(artist_id)
+	artist_dict: Dict = {}
+	artist_dict['name'] = artist.name
+	artist_dict['images'] = artist.images
+	artist_dict['url'] = 'open.spotify.com/artist/' + artist.id;
+	artist_dict['genres'] = artist.genres
+	artist_dict['followers'] = artist.followers
+	return Response(json.dumps(artist_dict), mimetype='text/json')
+
+
+async def get_artist_dict(artist_id):
+	artist: Artist = await clients.spotify.get_artist(artist_id)
+	artist_dict: Dict = {}
+	artist_dict['name'] = artist.name
+	artist_dict['images'] = artist.images
+	artist_dict['url'] = 'open.spotify.com/artist/' + artist.id;
+	artist_dict['genres'] = artist.genres
+	artist_dict['followers'] = artist.followers
+	return artist_dict
+
 
 if __name__ == '__main__':
 	clients.redis = redis.Redis()
